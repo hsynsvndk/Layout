092117b06996e5b07ad6165aebea7a9d27b2b2a1
“hsynsvndk” <hsynsvndk@hotmail.com>
2024-01-15 14:40:38 +0100

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'boxmodel';
+  innerBoxesForTitle2 = ['İç Kutu 1', 'İç Kutu 2', 'İç Kutu 3', 'İç Kutu 4', 'İç Kutu 5', 'İç Kutu 6', 'İç Kutu 7', 'İç Kutu 8', 'İç Kutu 9'];
+
 }
